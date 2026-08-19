ccd6fc23b0ed0aaba2e917c260d775610b316e1a
voquochung2369-creator <voquochung2369@gmail.com>
2026-08-20 02:04:49 +0800

    ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -30,32 +30,21 @@ end
 --------------------------------------------------
 
 ESP:AddButton({
-
     Name = "ESP Selector",
-
     Click = "Scroll",
-
     Slot = 1,
-
     Min = 0,
-
     Max = 1,
-
     Save = true,
-        
     Options = {
-
         {
             Name = "Player",
-
             Callback = function(Name, Selected)
-
                 if not _G.CustomHubESP then
-
                     local source = game:HttpGet(
                         "https://raw.githubusercontent.com/voquochung2369-creator/mainhub.lua/refs/heads/main/HotBar_ESP_Player.lua"
                     )
-
+                        
                     local fn = loadstring(source)
 
                     if fn then
