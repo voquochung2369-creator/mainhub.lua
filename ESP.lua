dad1efbabcdf6a0835f96a16eeb8408c9f8bc66e
voquochung2369-creator <voquochung2369@gmail.com>
2026-08-20 00:35:28 +0800

    ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -66,26 +66,16 @@ ESP:AddButton({
 Callback = function(Value)
 
     local source =
-        game:HttpGet(
-            "LINK_ESP_PLAYER.lua"
-        )
+        game:HttpGet("https://raw.githubusercontent.com/voquochung2369-creator/mainhub.lua/refs/heads/main/HotBar_ESP_Player.lua")
 
     local fn =
         loadstring(source)
-
     fn()
 
-
     if _G.CustomHubESP then
-
-        _G.CustomHubESP.Toggle(
-            Value
-        )
-
+        _G.CustomHubESP.Toggle(Value)
     end
-
 end
-})
 
 --------------------------------------------------
 -- ESP SELECTOR
