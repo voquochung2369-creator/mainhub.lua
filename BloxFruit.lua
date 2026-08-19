ed402a1cbeb329ae5e3be942c89b765af8cd29b7
voquochung2369-creator <voquochung2369@gmail.com>
2026-08-20 00:14:45 +0800

    Bloxfruit.lua

--- BloxFruit.lua
+++ BloxFruit.lua
@@ -203,3 +203,67 @@ Bloxfruit:AddButton({
             end
         end
 })
+
+--------------------------------------------------
+-- Auto Join Team Marines/Pirates
+--------------------------------------------------
+
+Bloxfruit:AddButton({
+    Name = "Auto Join Team",
+    Click = "Scroll",
+    Slot = 1,
+    Min = 0,
+    Max = 1,
+    Save = true,
+    Options = {
+            {
+            Name = "Marines",
+            Callback =
+                function(Name,Selected)
+                    getgenv().Team = "Marines"
+                end
+        },
+
+        {
+            Name = "Pirates",
+            Callback =
+                function(Name,Selected)
+                    getgenv().Team = "Pirates"
+                end
+        }
+    },
+
+    ChildClick = "Lever",
+    ChildName = "Auto Join Team",
+    ChildSave = true,
+    ChildDefault = false,
+    ChildCallback = function(
+            Enabled,
+            Selected
+        )
+
+        if Enabled then
+              task.wait(2) 
+            if Selected[1] == "Marines" then
+                getgenv().Team = "Marines"
+                game:GetService("ReplicatedStorage")
+                    .Remotes
+                    .CommF_:InvokeServer(
+                        "SetTeam",
+                        "Marines"
+                    )
+
+
+            elseif Selected[1] == "Pirates" then
+                task.wait(2)
+                getgenv().Team = "Pirates"
+                game:GetService("ReplicatedStorage")
+                    .Remotes
+                    .CommF_:InvokeServer(
+                        "SetTeam",
+                        "Pirates"
+                    )
+            end
+        end
+    end
+})
