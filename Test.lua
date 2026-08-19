272e9fb8a3a6b08b2d4676f7ecbb12f380fffdba
voquochung2369-creator <voquochung2369@gmail.com>
2026-08-19 22:54:24 +0800

    Test.lua

--- Test.lua
+++ Test.lua
@@ -1,1 +1,85 @@
-local Hub = _G.CustomHub
+local Hub =
+    _G.CustomHub
+
+if not Hub then
+
+    warn(
+        "CustomHub Main.lua chưa được load"
+    )
+
+    return
+end
+
+local Window =
+    Hub.Window
+
+--------------------------------------------------
+-- FIND TAB
+--------------------------------------------------
+
+local Test = nil
+
+for _, tab in ipairs(
+    Window.Tabs
+) do
+
+    if tab.Name ==
+        "Test" then
+
+        Setting =
+            tab
+
+        break
+    end
+end
+
+if not Setting then
+
+    warn(
+        "Không tìm thấy tab Test"
+    )
+
+    return
+end
+
+Setting:AddButton({
+    Name = "Auto Join Marines",
+    Click = "Lever",
+    Slot = 1,
+    Default = false,
+    Save = true,
+    Callback =
+        function(Value)
+            if Value then
+                local success, result =
+                    pcall(
+                        function()
+                            local source = getgenv().Team = "Marines"
+            )
+                            local fn =
+                                loadstring(
+                                    source
+                                )
+
+                            if type(fn)
+                                ~= "function" then
+
+                                error(
+                                    "Auto Join Marines fail"
+                                )
+                            end
+
+                            return fn()
+                        end
+                    )
+
+                if not success then
+
+                    warn(
+                        "Auto Join Marines error:",
+                        result
+                    )
+                end
+            end
+        end
+})
