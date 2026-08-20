455fc8fe0ab85582c15fffd3a4d015057e8f998b
voquochung2369-creator <voquochung2369@gmail.com>
2026-08-20 21:40:24 +0700

    BloxFruit.lua

--- BloxFruit.lua
+++ BloxFruit.lua
@@ -209,9 +209,7 @@ Bloxfruit:AddButton({
                     function()
 
                         local source =
-                            game:HttpGet(
-                                "https://raw.githubusercontent.com/hermanos-dev/hermanos-hub/refs/heads/main/Loader.lua"
-                            )
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/hermanos-dev/hermanos-hub/refs/heads/main/Loader.lua"))()
 
                         local fn =
                             loadstring(
